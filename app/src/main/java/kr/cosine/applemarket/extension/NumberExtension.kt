22e71b266a989a401abeb1d068ab87fb0440459c
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-04-15 18:41:00 +0900

    feat: DecimalFormat 포맷 수정

--- app/src/main/java/kr/cosine/applemarket/extension/NumberExtension.kt
+++ app/src/main/java/kr/cosine/applemarket/extension/NumberExtension.kt
@@ -2,6 +2,6 @@ package kr.cosine.applemarket.extension
 
 import java.text.DecimalFormat
 
-private val decimalFormat = DecimalFormat("#,##0")
+private val decimalFormat = DecimalFormat("#,###")
 
 fun Int.applyComma(): String = decimalFormat.format(this)
